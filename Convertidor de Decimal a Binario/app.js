632b0f6bd33d8f3eceaeda35ea62e30bb6498f78
Julian Lobon <julitolos6@gmail.com>
2024-04-11 17:07:24 -0500

    Convertidor de  binario

---
+++ Convertidor de Decimal a Binario/app.js
@@ -0,0 +1,34 @@
+const numberInput = document.getElementById("number-input");
+const convertBtn = document.getElementById("convert-btn");
+const result = document.getElementById("result");
+
+function decimalToBinary(input) {
+  let binary = ""
+  
+  if (input === 0) {
+    binary = "0"
+  } 
+
+  while (input > 0) {
+    binary = (input % 2) + binary
+    input = Math.floor(input / 2)
+   
+  }
+  result.innerText = binary 
+}
+
+  function checkUserInput() {
+  if (!numberInput.value || isNaN(parseInt(numberInput.value))) {
+   alert("Please provide a decimal number")
+   return
+  }
+  decimalToBinary(parseInt(numberInput.value));
+  numberInput.value = "";
+
+}
+
+convertBtn.addEventListener("click", checkUserInput); 
+numberInput.addEventListener("keydown", (e) => {
+  if (e.key === "Enter") {checkUserInput()};
+});
+
